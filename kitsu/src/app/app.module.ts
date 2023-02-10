23e007fb24af47e52458a27ff6d00b2551fab155
Stefanie Silva <matsustefanie@gmail.com>
2023-02-09 23:37:16 -0300

    inicio slide de categorias

--- kitsu/src/app/app.module.ts
+++ kitsu/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CategoryComponent } from './category/category.component';
+import { SlideComponent } from './slide/slide.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CategoryComponent,
+    SlideComponent
   ],
   imports: [
     BrowserModule,
